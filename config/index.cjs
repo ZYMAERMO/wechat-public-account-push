008441cc238edb85df40bd0897efe720fd46e210
ZYMAERMO <34448486+ZYMAERMO@users.noreply.github.com>
2024-12-06 17:17:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,7 +44,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oDDOG6Xp1VZiAAwPV9fXTEuJWUo4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'u8MbpLwub2BJC5_pQU4rXVxpZzvp9xwzXiJADNYwA5M',
+      useTemplateId: 'q0QamqUoBFF89bdwDxbyH9U97qSi_XHK6dMJZ1rfTOc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '08-10',
       festivals: [
