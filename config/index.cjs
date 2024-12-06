9f8c389b560cfa698367e36c54e7c1fb85c21288
ZYMAERMO <34448486+ZYMAERMO@users.noreply.github.com>
2024-12-06 17:49:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -40,7 +40,7 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '臭臭',
+      name: '自己',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oDDOG6Xp1VZiAAwPV9fXTEuJWUo4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
