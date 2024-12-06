a5066035b2de1865ba285fda0a892d040a0c7609
ZYMAERMO <34448486+ZYMAERMO@users.noreply.github.com>
2024-12-06 17:28:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -72,7 +72,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'bqK7Pu9kpnfkCTOoeGoaocr7mW-5AF17zwPru2CFx9k',
 
   CALLBACK_USERS: [
     {
